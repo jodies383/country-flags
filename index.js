cc82a19f664673b87654c33e6f65ccbc6f9c8c2b
Jodie Solomons <jodies383@gmail.com>
2022-02-25 15:36:33 +0200

    new features

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const templateCountries = document.querySelector('.countries').innerHTML
-
+const templateCountries = document.querySelector(".countries").innerHTML
+const templateFlags = document.querySelector(".flags").innerHTML
 const countriesTemplate = Handlebars.compile(templateCountries)
-
+const flagsTemplate = Handlebars.compile(templateFlags)
 const countries = document.querySelector(".displayCountries")
+const flags = document.querySelector(".displayFlags")
 
 const countryList = countriesTemplate({
     country: ["Argentina", "Brazil", "Chile", "Zambia", "Uganda", "Malawi", "Rwanda", "Ireland", "Switzerland"],
+
+});
+const flagList = flagsTemplate({
     flag: ["🇦🇷", "🇧🇷", "🇨🇱", "🇿🇲", "🇺🇬", "🇲🇼", "🇷🇼", "🇮🇪", "🇨🇭"]
 
 });
+//use loop or create array of objects
 countries.innerHTML = countryList;
+flags.innerHTML = flagList;
+//loop over, push into new list
+console.log(typeof(countryList));
